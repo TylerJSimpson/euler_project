ed9d20a3c06f82354711d361cfb982dbbe3cc3f8
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-01-31 20:52:27 -0500

    Update 3. Largest prime factor.py

--- archive/python/3. Largest prime factor.py
+++ archive/python/3. Largest prime factor.py
@@ -1,24 +1,23 @@
 #3. Largest prime factor
 
 def largest_prime_factor(num: int):
-    
-    #create variables
-    n = 2
-    factors = []
-    #account for num 1
-    if num == 1:
+    """
+    largest_prime_factor sums the fibonnaci numbers up to the given int n input
+    num: integer
+    """   
+    n = 2                                   #set counter n = 2
+    factors = []                            #create list to hold factors
+    if num == 1:                            #account for num = 1
         return 1
-    #add prime factors to factors list
-    while n ** 2 <= num:
+    while n ** 2 <= num:                    #use square root of n to quickly remove options above largest prime factor
         if num % n == 0:
             factors.append(n)
             num //= n
         else:
             n += 1
     if num > 1:
-       factors.append(num)
-    #return the highest prime factor
-    return max(factors)
+       factors.append(num)                  #append prime factors
+    return max(factors)                     #return max prime factor
 
 n = 600851475143
 print(largest_prime_factor(n))
